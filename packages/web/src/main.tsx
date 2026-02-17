d9c582e746f4efef60acced56737616bd9d1189d
tripavail92 <tripavail92@gmail.com>
2026-02-18 00:25:42 +0500

    Force rebuild with code change

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -7,7 +7,9 @@ import App from './App.tsx'
 import { GlobalErrorBoundary } from './components/GlobalErrorBoundary'
 import { ThemeProvider } from './theme/ThemeContext'
 
-// Force build refresh - 2026-02-17
+// Force cache invalidation - 2026-02-18
+console.info('TripAvail Web v2.0.1')
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <GlobalErrorBoundary>
     <ThemeProvider defaultMode="system" storageKey="tripavail-theme-mode">
