7d51dc4a937edaf837d923b212620918960092e7
tripavail92 <tripavail92@gmail.com>
2026-02-17 20:51:38 +0500

    Remove React.StrictMode for production deployment

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -8,12 +8,10 @@ import { GlobalErrorBoundary } from './components/GlobalErrorBoundary'
 import { ThemeProvider } from './theme/ThemeContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GlobalErrorBoundary>
-      <ThemeProvider defaultMode="system" storageKey="tripavail-theme-mode">
-        <App />
-      </ThemeProvider>
-    </GlobalErrorBoundary>
-  </React.StrictMode>,
+  <GlobalErrorBoundary>
+    <ThemeProvider defaultMode="system" storageKey="tripavail-theme-mode">
+      <App />
+    </ThemeProvider>
+  </GlobalErrorBoundary>,
 )
 
